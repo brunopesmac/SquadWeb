568d4cad370965540154ac8e254112938be3e4bb
Caio Nobile <caio.nobile51@gmail.com>
2020-11-08 16:02:56 -0300

    ArrayList de pontos (Classe Viagem)

--- SquadWeb/src/model/Viagem.java
+++ SquadWeb/src/model/Viagem.java
@@ -1,40 +1,56 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Viagem {
 	
 	int id;
-	String partida;
-	String chegada;
+	String partida, chegada;
+	ArrayList<String> pontos = new ArrayList<String>();
 	
-	public Viagem(int id,String partida, String chegada) {
-		this.id=id;
-		this.partida=partida;
-		this.chegada=chegada;
+	public Viagem(int id, String partida, String chegada, ArrayList<String> pontos) {
+		this.id = id;
+		this.partida = partida;
+		this.chegada = chegada;
+		this.pontos = pontos;
 	}
-	public Viagem (int id) {
-		this.id=id;
-	}
-	public Viagem() {}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getPartida() {
 		return partida;
 	}
+
 	public void setPartida(String partida) {
 		this.partida = partida;
 	}
+
 	public String getChegada() {
 		return chegada;
 	}
+
 	public void setChegada(String chegada) {
 		this.chegada = chegada;
 	}
+
+	public ArrayList<String> getPontos() {
+		return pontos;
+	}
+
+	public void setPontos(ArrayList<String> pontos) {
+		this.pontos = pontos;
+	}
+
 	@Override
 	public String toString() {
-		return "Viagem [id=" + getId() + ", partida=" + getPartida() +", chegada=" + getChegada() + "]";
+		return "Viagem [id=" + id + ", partida=" + partida + ", chegada=" + chegada + ", pontos=" + pontos.toString() + "]";
 	}
+	
+	
 }
